dee4d2b0fb2c32b349606795ed61ddad0adf4be5
Nusrat Jahan Dipa <nusratjd41@gmail.com>
2026-09-11 07:49:20 -0700

    Update code

--- 03-middleware/index.js
+++ 03-middleware/index.js
@@ -5,11 +5,8 @@ const app = express();
 const PORT = 3000;
 
 
-// JSON body
 app.use(express.json());
 
-
-// ---------- Custom Middleware 1: Logger ----------
 const logger = (req, res, next) => {
 
     console.log(`${req.method} request to ${req.url}`);
@@ -19,3 +16,37 @@ const logger = (req, res, next) => {
 
 app.use(logger);
 
+const checkAuth = (req, res, next) => {
+
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).send('No token, access denied');
+    }
+
+    console.log('Token found:', token);
+
+    next();
+};
+
+
+// ---------- Routes ----------
+
+app.get('/', (req, res) => {
+    res.send('Hello Dipa!');
+});
+
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+
+app.get('/dashboard', checkAuth, (req, res) => {
+    res.send('Welcome to Dashboard!');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
