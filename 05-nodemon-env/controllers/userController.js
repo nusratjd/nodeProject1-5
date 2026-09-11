a58ee859453bf74ed77ec71cdd4eddb6c3dbe900
Nusrat Jahan Dipa <nusratjd41@gmail.com>
2026-09-11 08:11:29 -0700

    Update code

---
+++ 05-nodemon-env/controllers/userController.js
@@ -0,0 +1,27 @@
+const createUser = (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+
+    res.send(`User created: ${name}, ${email}`);
+};
+
+const updateUser = (req, res) => {
+    res.send('User updated');
+};
+
+const deleteUser = (req, res) => {
+    res.send('User deleted');
+};
+
+const getUserById = (req, res) => {
+    const userId = req.params.id;
+
+    res.send(`User ID: ${userId}`);
+};
+
+module.exports = {
+    createUser,
+    updateUser,
+    deleteUser,
+    getUserById,
+};
